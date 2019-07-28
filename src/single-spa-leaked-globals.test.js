743b5f676af5c569c54332965d751022906291d6
Joel Denning <joeldenning@gmail.com>
2019-07-28 13:20:56 -0600

    Initial implementation

---
+++ src/single-spa-leaked-globals.test.js
@@ -0,0 +1,32 @@
+import singleSpaLeakedGlobals from "./single-spa-leaked-globals";
+
+describe("single-spa-leaked-globals", () => {
+  it(`adds the global vars during mount and unmounts them during unmount`, () => {
+    const lifecycles = singleSpaLeakedGlobals({
+      globalVariableNames: ["foo"]
+    });
+
+    console.log("lifecycles", lifecycles);
+
+    window.foo = "foo";
+
+    return lifecycles.bootstrap().then(() => {
+      expect(window.foo).toBe("foo");
+      return lifecycles
+        .mount()
+        .then(() => {
+          expect(window.foo).toBe("foo");
+          return lifecycles.unmount();
+        })
+        .then(() => {
+          expect(window.foo).toBe(undefined);
+          return lifecycles.mount();
+        })
+        .then(() => {
+          {
+            expect(window.foo).toBe("foo");
+          }
+        });
+    });
+  });
+});
